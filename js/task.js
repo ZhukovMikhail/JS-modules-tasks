42ad4aa440ab6ceaafbf9589738f5e892bec6742
ZhukovMikhail <79285451+ZhukovMikhail@users.noreply.github.com>
2021-06-04 21:28:33 +0300

    tasks of mod 4 10/10 done
    mod 4 10/10 done

--- js/task.js
+++ js/task.js
@@ -1657,25 +1657,120 @@
 // Объявлена переменная messages.
 // Значение переменной messages это массив ['Готовим Burger для solomon@topmail.ua. Ваш заказ 1-й в очереди.', 'Готовим Pizza для artemis@coldmail.net. Ваш заказ 2-й в очереди.', 'Готовим Taco для jacob@mail.com. Ваш заказ 3-й в очереди.'].
 
-const orders = [
-  { email: 'solomon@topmail.ua', dish: 'Burger' },
-  { email: 'artemis@coldmail.net', dish: 'Pizza' },
-  { email: 'jacob@mail.com', dish: 'Taco' },
-];
-// // Пиши код ниже этой строки
-// function composeMessage(position) {}
+// const orders = [
+//   { email: 'solomon@topmail.ua', dish: 'Burger' },
+//   { email: 'artemis@coldmail.net', dish: 'Pizza' },
+//   { email: 'jacob@mail.com', dish: 'Taco' },
+// ];
+// // // Пиши код ниже этой строки
+// // function composeMessage(position) {}
 
+// // const messages = [];
+// //------------------------------------------------------
+
+
+// function composeMessage(position) {
+//   return `Готовим ${this.dish} для ${this.email}. Ваш заказ ${position}-й в очереди.`;
+// }
 // const messages = [];
+// for (let i = 0; i < orders.length; i += 1) {
+//   messages.push(composeMessage.call(orders[i], i + 1)); //call
+//   messages.push(composeMessage.apply(orders[i], [i + 1]))//apply
+// }
+
+// console.log(messages)
 //------------------------------------------------------
 
+// ================  Метод bind  ==============================
+// Методы call и apply вызывают функцию «на месте», то есть сразу. Но в случае колбэк-функций, когда необходимо не сразу вызвать функцию, а передать ссылку на неё, причём с привязанным контекстом, используется метод bind.
 
-function composeMessage(position) {
-  return `Готовим ${this.dish} для ${this.email}. Ваш заказ ${position}-й в очереди.`;
-}
-const messages = [];
-for (let i = 0; i < orders.length; i += 1) {
-  messages.push(composeMessage.call(orders[i], i + 1)); //call
-  messages.push(composeMessage.apply(orders[i], [i + 1]))//apply
+// foo.bind(obj, arg1, arg2, ...)
+// Метод bind создаёт и возвращает копию функции foo с привязанным контекстом obj и аргументами arg1, arg2 и т. д. Получается копия функции которую можно передать куда угодно и вызвать когда угодно.
+
+function greet(clientName) {
+  return `${clientName}, добро пожаловать в «${this.service}».`;
 }
 
-console.log(messages)
+const steam = { service: 'Steam' };
+const steamGreeter = greet.bind(steam);
+
+console.log(steamGreeter('Манго')); // "Манго, добро пожаловать в «Steam»."
+
+const gmail = { service: 'Gmail' };
+const gmailGreeter = greet.bind(gmail);
+
+console.log(gmailGreeter('Поли')); // "Поли, добро пожаловать в «Gmail»."
+//-------------------------------------------------------------------
+
+// ================  Метод bind  и методы объекта ==============================
+
+// При передаче методов объекта как колбэк-функций, контекст не сохраняется. Колбэк это ссылка на метод, которая присваивается как значение параметра, вызываемого без объекта.
+
+// const customer = {
+//   firstName: 'Jacob',
+//   lastName: 'Mercer',
+//   getFullName() {
+//     return `${this.firstName} ${this.lastName}`;
+//   },
+// };
+
+// function makeMessage(callback) {
+//   // callback() это вызов метода getFullName без объекта
+//   console.log(`Обрабатываем заявку от ${callback()}.`);
+// }
+
+// makeMessage(customer.getFullName); // Будет ошибка при вызове функции
+// В строгом режиме, значение this в методе getFullName, при вызове как колбэк-функции callback(), будет undefined. При обращении к свойствам firstName и lastName будет ошибка, так как undefined это не объект.
+
+// Метод bind используется для привязки контекста при передаче методов объекта как колбэк-функций. Передадим колбэком не оригинальный метод getFullName, а его копию с привязанным контекстом к объекту customer.
+
+// // ❌ Было
+// makeMessage(customer.getFullName); // Будет ошибка при вызове функции
+
+// // ✅ Стало
+// makeMessage(customer.getFullName.bind(customer)); // Обрабатываем заявку от Jacob Mercer.
+//-------------------------------------------------------------------
+
+// Задание
+// Сервису рассылки электронной почты необходимо добавить логирование действий для сбора статистики. Функция logAndInvokeAction(email, action) ожидает почту и действие которое нужно выполнить - ссылку на метод объекта service. Сбор статистики симулируется логированием строки. Разберись и дополни код так, чтобы он работал верно.
+
+// Тесты
+// Объявлена переменная service.
+// Значение переменной service это оригинальный объект.
+// Объявлена функция logAndInvokeAction(email, action).
+// Переменной firstInvoke присвоена строка 'Почта kiwi@mail.uk добавлена в рассылку.'.
+// Первый вызов logAndInvokeAction с почтой kiwi@mail.uk и методом service.subscribe привязан к объекту service.
+// Переменной secondInvoke присвоена строка 'Почта poly@hotmail.de удалена из рассылки.'.
+// Второй вызов logAndInvokeAction с почтой poly@hotmail.de и методом service.unsubscribe привязан к объекту service.
+
+const service = {
+  mailingList: ['mango@mail.com', 'poly@hotmail.de', 'ajax@jmail.net'],
+  subscribe(email) {
+    this.mailingList.push(email);
+    return `Почта ${email} добавлена в рассылку.`;
+  },
+  unsubscribe(email) {
+    this.mailingList = this.mailingList.filter((e) => e !== email);
+    return `Почта ${email} удалена из рассылки.`;
+  },
+};
+
+function logAndInvokeAction(email, action) {
+  console.log(`Выполняем действие с ${email}.`);
+  return action(email);
+}
+
+const firstInvoke = logAndInvokeAction('kiwi@mail.uk', service.subscribe.bind(service);
+console.log(firstInvoke);
+// Почта kiwi@mail.uk добавлена в рассылку.
+
+console.log(service.mailingList);
+/* ['mango@mail.com', 
+    'poly@hotmail.de', 
+    'ajax@jmail.net', 
+    'kiwi@mail.uk']*/
+const secondInvoke = logAndInvokeAction('poly@hotmail.de', service.unsubscribe.bind(service));
+console.log(secondInvoke);
+// Почта poly@hotmail.de удалена из рассылки.
+
+console.log(service.mailingList); // ['mango@mail.com', 'ajax@jmail.net', 'kiwi@mail.uk']
